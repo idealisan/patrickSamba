66a9a77ea33f37c3bfe6af19989cd715238f27df
FinalTape <xhFrbRYmdSS2TXsOGpOYoD+FinalTape@noreply.cnb.cool>
2026-08-08 23:25:19 +0800

    mdns: 组播 socket（224.0.0.251/[ff02::fb]:5353，TTL=255，SO_REUSEADDR/PORT）

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.25.0
 
 require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 )
---
+++ internal/mdns/sockopt_unix.go
@@ -0,0 +1,33 @@
+//go:build !windows
+
+package mdns
+
+import (
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+// setReuse 允许多个套接字绑定同一个 UDP 端口。
+//
+// 5353 端口上很可能已经有系统的 mDNS 实现（avahi / mDNSResponder）在跑。
+// 我们**不与它通信**（AGENTS.md C4 禁止），只是不去抢占端口，
+// 让两者可以各自独立收发组播报文。
+//
+// 纯 Go：golang.org/x/sys/unix 是系统调用号封装，不需要 CGO。
+func setReuse(network, address string, c syscall.RawConn) error {
+	var sockErr error
+	err := c.Control(func(fd uintptr) {
+		if err := unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
+			sockErr = err
+			return
+		}
+		// SO_REUSEPORT 在 Linux 3.9+ / BSD / macOS 上才有；
+		// 拿不到不影响组播接收，SO_REUSEADDR 才是关键的那一个，因此忽略错误。
+		_ = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	})
+	if err != nil {
+		return err
+	}
+	return sockErr
+}
